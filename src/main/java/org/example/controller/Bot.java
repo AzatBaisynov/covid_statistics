35d9625cdeed778c5b85be7667a10ac50e196e86
AzatBaisynov <azat.baisynov@gmail.com>
2020-04-17 02:09:09 +0600

    ''

--- src/main/java/org/example/controller/Bot.java
+++ src/main/java/org/example/controller/Bot.java
@@ -1,5 +1,6 @@
 package org.example.controller;
 
+import com.vdurmont.emoji.EmojiParser;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
@@ -20,16 +21,21 @@ import static org.example.controller.BotUpdate.runUpdate;
 public class Bot extends TelegramLongPollingBot {
 
     public void sendMsgWithAdv(Message message, String text) throws FileNotFoundException {
+        String parsedSmile = EmojiParser.parseToUnicode(":point_right:");
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setText(text);
-        SendPhoto msg = new SendPhoto().setPhoto("место для Вашей рекламы!", new FileInputStream(new File("classes/adv.jpg")));
+        SendPhoto msg = new SendPhoto().setPhoto("место для Вашей рекламы!", new FileInputStream(new File("classes/rec.jpg")));
         msg.setChatId(message.getChatId().toString());
-
-
+        SendMessage sendMessage1 = new SendMessage();
+        sendMessage1.setText("Только грамотные треки в стиле кайфа " + parsedSmile+ parsedSmile
+                + parsedSmile+ parsedSmile+ " @musicaproanima");
+        sendMessage1.setChatId(message.getChatId().toString());
+        sendMessage1.enableMarkdown(true);
         try {
             execute(msg);
+            execute(sendMessage1);
             execute(sendMessage);
         } catch (TelegramApiException e) {
             e.printStackTrace();
