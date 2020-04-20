3d8fe8a60631553ca46bf8869b769926d10648df
AzatBaisynov <azat.baisynov@gmail.com>
2020-04-21 03:03:28 +0600

    ''

--- src/main/java/org/example/controller/Bot.java
+++ src/main/java/org/example/controller/Bot.java
@@ -26,7 +26,7 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setText(text);
-        SendPhoto msg = new SendPhoto().setPhoto("место для Вашей рекламы!", new FileInputStream(new File("src/main/resources/rec.jpg")));
+        SendPhoto msg = new SendPhoto().setPhoto("место для Вашей рекламы!", new FileInputStream(new File("classes/rec.jpg")));
         msg.setChatId(message.getChatId().toString());
         SendMessage sendMessage1 = new SendMessage();
         sendMessage1.setText("Только грамотные треки в стиле кайфа " + parsedSmile+ parsedSmile
@@ -47,7 +47,7 @@ public class Bot extends TelegramLongPollingBot {
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId().toString());
         sendMessage.setText(text);
-        SendPhoto msg = new SendPhoto().setPhoto("Статистика", new FileInputStream(new File("src/main/resources/kyrg.jpg")));
+        SendPhoto msg = new SendPhoto().setPhoto("Статистика", new FileInputStream(new File("classes/kyrg.jpg")));
         msg.setChatId(message.getChatId().toString());
         try {
             execute(msg);
